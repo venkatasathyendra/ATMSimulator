3eb3b74d58d319410c9720d9da6ba07a3f9e2e6a
venkatasathyendra <venkatasathyendra@gmail.com>
2019-10-03 21:23:42 +0530

    Add files via upload

--- ATMSimulator/src/com/simulator/atm/pojos/Transaction.java
+++ ATMSimulator/src/com/simulator/atm/pojos/Transaction.java
@@ -1,12 +1,15 @@
 package com.simulator.atm.pojos;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Transaction {
 	 private double dollars;
-	 private Date tx_date;
+	 private LocalDate tx_date;
 	 private String tx_type;
 	 private double closing_balance;
+	 private double txn_amt;
 	 	 
 
 	public double getDollars() {
@@ -25,12 +28,12 @@ public class Transaction {
 		this.tx_type = tx_type;
 	}
 
-	public Date getTx_date() {
+	public LocalDate getTx_date() {
 		return tx_date;
 	}
 
-	public void setTx_date(Date tx_date) {
-		this.tx_date = tx_date;
+	public void setTx_date(LocalDate now) {
+		this.tx_date = now;
 	}
 
 	public double getBalance() {
@@ -39,6 +42,14 @@ public class Transaction {
 
 	public void setBalance(double balance) {
 		this.closing_balance = balance;
+	}
+
+	public double getTxn_amt() {
+		return txn_amt;
+	}
+
+	public void setTxn_amt(double txn_amt) {
+		this.txn_amt = txn_amt;
 	}
 	 
 
--- ATMSimulator/src/com/simulator/atm/utility/ATMOperationUtility.java
+++ ATMSimulator/src/com/simulator/atm/utility/ATMOperationUtility.java
@@ -1,10 +1,14 @@
 package com.simulator.atm.utility;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 import com.simulator.atm.ATMInterface;
 import com.simulator.atm.pojos.Currency;
@@ -13,9 +17,11 @@ import com.simulator.atm.pojos.Transaction;
 public class ATMOperationUtility implements ATMInterface {
 	
 	Currency currency = new Currency();
-	Transaction txn = new Transaction();
-	double closingBal = 0;
-	public List<Transaction> txnLst;
+	
+	static double closingBal = 0;
+	public List<Transaction> txnLst = new ArrayList<Transaction>();
+	
+	int denominations[] = {10,20,20};
 	
 	public void PrintMenuOptions() {
 		
@@ -38,19 +44,15 @@ public class ATMOperationUtility implements ATMInterface {
 	@Override
 	public double deposit(double curr) {
 		
-	    currency.setBalance(curr);
-		System.out.println("Available balance post deposit is : "+currency.getBalance());
-		
-		return currency.getBalance();
+	   closingBal = closingBal + curr;
+	   System.out.println("Available balance post deposit is : "+closingBal);
+		return closingBal;
 	}
 
 	@Override
 	public double withdraw(double curr) {
 		
-		closingBal = currency.getBalance() - curr;
-		
-		System.out.println("curr bal in withdraw"+currency.getBalance());
-		System.out.println("with draw amt : "+closingBal);
+		closingBal = closingBal - curr;
 		currency.setBalance(closingBal);
 		System.out.println("Available balance post withdraw is : "+currency.getBalance());
 		return closingBal;
@@ -66,7 +68,7 @@ public class ATMOperationUtility implements ATMInterface {
 		}else 
 		{
 			for (Transaction tx : lst) {
-				System.out.println(tx.getTx_date() + tx.getTx_type() + tx.getBalance() );
+				System.out.println(tx.getTx_date() +"     |       " + tx.getTx_type() +"        |       "+ tx.getDollars() + "       |       " +tx.getBalance());
 			}
 		}
 		
@@ -75,8 +77,8 @@ public class ATMOperationUtility implements ATMInterface {
 
 	@Override
 	public double balance() {
-		System.out.println("Available Balance is : "+currency.getBalance());
-		return currency.getBalance();
+		System.out.println("Available Balance is : "+closingBal);
+		return closingBal;
 	}
 	
 	public Scanner ATMOperations(int action) {
@@ -85,7 +87,10 @@ public class ATMOperationUtility implements ATMInterface {
 		Date timePeriod = null;
 		double curr = 0 ;
 		double notes = 0;
-			
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");  
+		LocalDate now = LocalDate.now();  
+		Transaction txn = new Transaction();
+		 
 		ATMOperationUtility atmOperations = new ATMOperationUtility();
 		switch (action) 
 		{
@@ -93,33 +98,42 @@ public class ATMOperationUtility implements ATMInterface {
 			case 1 : 
 				System.out.println("Enter the amount which you wanted to Deposit : ");
 				notes = scanner.nextDouble();
-				if (notes == 0.0)
-				{
-					System.out.println("0.0 not accepted for Deposit; Please enter valid amount");
-					break;
+				String nt = scanner.nextLine();
+				StringTokenizer st = new StringTokenizer(nt);
+				int dollar_notes[] = null;int i =0;
+				while (st.hasMoreTokens()) {
+					String s = st.nextToken("");
+					int val = Integer.parseInt(s);
+					dollar_notes[i] = val;
 				}
+				System.out.println("deposit denominations "+notes);
+				for (int i1 : dollar_notes)
+					System.out.println(i1);
+				
 				
 				curr = atmOperations.deposit(notes);
-				currency.setBalance(curr);
-
-//				txn.setBalance(curr);
-//				txn.setDollars(notes);
-//				txn.setTx_type("Deposit");
-//				txn.setTx_date(new Date("12/09/2019"));
-//				txnLst.add(txn);
-				
+				txn.setBalance(closingBal);
+				txn.setTxn_amt(curr);
+				txn.setDollars(notes);
+				txn.setTx_type("Deposit");
+				txn.setTx_date(now);
+				txnLst.add(txn);
+				System.out.println("Closing Balance: after deposit :"+closingBal);
 				break;
 				
 			case 2: 
 				System.out.println("Enter the amount to withdraw  : ");
 				notes = scanner.nextDouble();
 				curr = atmOperations.withdraw(notes);
-				currency.setBalance(curr);
-//				txn.setBalance(curr);
-//				txn.setDollars(notes);
-//				txn.setTx_type("Withdraw");
-//				txn.setTx_date(new Date());
-//				txnLst.add(txn);
+				
+				txn.setBalance(closingBal);
+				txn.setTxn_amt(curr);
+				txn.setDollars(notes);
+				txn.setTx_type("Withdraw");
+				txn.setTx_date(now);
+				txnLst.add(txn);
+				
+				System.out.println("Closing Balance: after Withdraw :"+closingBal);
 				
 				break;
 			case 3: 
